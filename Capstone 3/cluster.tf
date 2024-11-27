fcde672eb3d5ebb9c35d33bf20008b64f6c1c781
comal21 <125987667+comal21@users.noreply.github.com>
2024-11-27 17:45:54 +0530

    Update cluster.tf

--- Capstone 3/cluster.tf
+++ Capstone 3/cluster.tf
@@ -2,6 +2,7 @@ resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   features {}
+  subscription_id  = "beca198a-e100-4ec3-8bff-e2ef65025f9b"
 }
 
 resource "azurerm_resource_group" "default" {
