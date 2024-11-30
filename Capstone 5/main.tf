b1e8b698fa7dc195aa959b5c06cac77c337a023f
comal21 <125987667+comal21@users.noreply.github.com>
2024-11-30 23:34:37 +0530

    Create main.tf

---
+++ Capstone 5/main.tf
@@ -0,0 +1,29 @@
+terraform {
+   cloud {
+    organization = "Terraformon"  #Add this block after running Terraform apply once
+    workspaces {
+      name = "TerraformOnAzure"
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
